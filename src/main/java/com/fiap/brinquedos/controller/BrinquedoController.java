836ef3bd0a005f49beb37b8e1fe5126763e44fb0
felipesora <felipeusora@gmail.com>
2025-05-05 16:10:37 -0300

    Criando método DELETE no controller

--- src/main/java/com/fiap/brinquedos/controller/BrinquedoController.java
+++ src/main/java/com/fiap/brinquedos/controller/BrinquedoController.java
@@ -1,8 +1,11 @@
 package com.fiap.brinquedos.controller;
 
 import com.fiap.brinquedos.model.Brinquedo;
+import com.fiap.brinquedos.model.ErrorResponse;
+import com.fiap.brinquedos.model.SuccessResponse;
 import com.fiap.brinquedos.service.BrinquedoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,5 +35,16 @@ public class BrinquedoController {
     @PutMapping("/{id}")
     public Brinquedo editar(@PathVariable Long id,@RequestBody Brinquedo brinquedo) {
         return service.editar(id, brinquedo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletar(@PathVariable Long id) {
+        String resposta = service.deletar(id);
+
+        if (resposta.equals("Brinquedo removido com sucesso!")) {
+            return ResponseEntity.ok(new SuccessResponse(resposta));
+        } else {
+            return ResponseEntity.status(404).body(new ErrorResponse(resposta));
+        }
     }
 }
