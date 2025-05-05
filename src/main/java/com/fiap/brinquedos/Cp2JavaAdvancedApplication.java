941906c79c43a3ad8bfc3f4ef1a36fadc9913413
felipesora <felipeusora@gmail.com>
2025-05-05 15:17:18 -0300

    Primeiro Commit

---
+++ src/main/java/com/fiap/brinquedos/Cp2JavaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.brinquedos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp2JavaAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp2JavaAdvancedApplication.class, args);
+	}
+
+}
